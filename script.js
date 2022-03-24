f0a2052750b3f4ed1abd60febf461390e3fca935
Eltaurus-Lt <93875472+Eltaurus-Lt@users.noreply.github.com>
2022-03-24 07:46:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-1
+var x0=400,y0=400;
+const {style} = obj;step=100;
+  
+window.addEventListener('keydown', (event) => {
+        
+    switch (event.key) {
+        case 'ArrowUp': y0+=step; break;
+        case 'ArrowDown': y0-=step; break;
+        case 'ArrowLeft': x0-=step; break;
+        case 'ArrowRight': x0+=step; break;
+    }
+
+    style.bottom=y0+`px`;
+    style.left=x0+`px`;
+   
+});
